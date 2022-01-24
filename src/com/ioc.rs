ba2b07e004bfdb1179a4e5bc338e498702f97f29
450220020@qq.com <q1q123456>
2022-01-24 10:06:22 +0800

    up  ioc

---
+++ src/com/ioc.rs
@@ -0,0 +1,12 @@
+use dashmap::DashMap;
+use once_cell::sync::OnceCell;
+use std::any::Any;
+
+
+
+
+#[test]
+fn test_ioc(){
+
+
+}
---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+extern crate dashmap;
+extern crate once_cell;
+pub mod com;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
+
+
+#[test]
+fn test_one(){
+
+}
